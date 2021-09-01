8ed816c8edadb02795e98194fb1cbb06281f3532
Ragillah <ragillahsyahp@gmail.com>
2021-09-01 18:38:53 +0700

    add animation and font

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,11 @@
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
-    origin: 'bottom',
-    distance: '80px',
-    duration: 1000,
-    reset: true
+   origin: "bottom",
+   distance: "80px",
+   duration: 1000,
+   reset: true,
 });
 
-sr.reveal('#title-genre',{}); 
-sr.reveal('.subtitle-services',{delay: 200});
-sr.reveal('#card',{delay: 400});
-// sr.reveal('.img-fluid',{ interval: 200}); 
+sr.reveal("#title-genre", {});
+sr.reveal("#card", { delay: 400 });
+sr.reveal(".card-img-top", { interval: 200 });
